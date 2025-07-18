01750b6082f1266cc814c9729b5ee14c411d2a39
PlaceHolderHere <holderhereinplace@gmail.com>
2025-07-18 22:21:11 +0800

    Added a function to get the new, rotated coordinated for a certain point based on the matrix multiplications of the generic rotation matrices in the reference post.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function multiplyMatrices(matrix1, matrix2){
-            if (matrix1.length <= 0){
-                return false
-            }else if(matrix2.length <= 0){
-                return false
-            }else if(matrix1[0].length != matrix2.length){
-                return false
+    if (matrix1.length <= 0){
+        return false
+    }else if(matrix2.length <= 0){
+        return false
+    }else if(matrix1[0].length != matrix2.length){
+        return false
+    }
+
+    output = []
+    // Iterate every row in matrix1
+    for (row_index = 0; row_index < matrix1.length; row_index++){
+        output.push([])
+        // Iterate over every col in matrix2
+        for (col_index = 0; col_index < matrix2[0].length; col_index++){
+            output_num = 0
+            // Iterate over every no. in matrix1[row_index] and get corresponding no. in col_index of matrix2
+            for (num_index = 0; num_index < matrix1[row_index].length; num_index++){
+                output_num += matrix1[row_index][num_index] * matrix2[num_index][col_index]
             }
-
-            output = []
-            // Iterate every row in matrix1
-            for (row_index = 0; row_index < matrix1.length; row_index++){
-                output.push([])
-                // Iterate over every col in matrix2
-                for (col_index = 0; col_index < matrix2[0].length; col_index++){
-                    output_num = 0
-                    // Iterate over every no. in matrix1[row_index] and get corresponding no. in col_index of matrix2
-                    for (num_index = 0; num_index < matrix1[row_index].length; num_index++){
-                        output_num += matrix1[row_index][num_index] * matrix2[num_index][col_index]
-                    }
-                    output[row_index].push(output_num)
-                }
-            } return output
-
+            output[row_index].push(output_num)
         }
+    } return output
+}
+
+function get_coordinates(angleA, angleB, phi, inner_radius, torus_radius){
+    // This is derived by multiplying all rotation matrices and the original coordinates
+    return [[],
+    [],
+    []]
+}
