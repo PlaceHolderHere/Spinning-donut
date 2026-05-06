da3d454e4ec9736a85eedb6db56a0ca5855c7829
PlaceHolderHere <holderhereinplace@gmail.com>
2026-05-06 23:10:45 +0800

    Each point can now independently store their own color

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Classes
 class Point:
-    def __init__(self, x, y, z, theta, phi, luminance):
+    def __init__(self, x, y, z, theta, phi, luminance, color):
         self.x = x
         self.y = y
         self.z = z
         self.theta = theta
         self.phi = phi
         self.luminance = luminance
+        self.color = color
 
 
 # Functions
@@ -52,7 +53,7 @@ def generate_torus() -> list[Point]:
             y = R1 * math.sin(theta)
             z = -((R2 + R1 * cos_theta) * sin_phi)
 
-            output.append(Point(x, y, z, theta, phi, 0))
+            output.append(Point(x, y, z, theta, phi, 0, COLOR))
             theta += theta_increment
         phi += phi_increment
     return output
@@ -80,7 +81,7 @@ def rotate_point(point: Point, angle_x: float, angle_y: float, angle_z: float) -
     luminance: float = (cos_phi * cos_theta * sinz) - (cosx * cos_theta * sin_phi) - (sinx * sin_theta) + (
                 cosz * (cosx * sin_theta - cos_theta * sinx * sin_phi))
 
-    return Point(x, y, z, point.theta, point.phi, luminance)
+    return Point(x, y, z, point.theta, point.phi, luminance, point.color)
 
 
 def rotate_points(points: list[Point], angle_x: float, angle_y: float, angle_z: float) -> list[Point]:
@@ -90,15 +91,15 @@ def rotate_points(points: list[Point], angle_x: float, angle_y: float, angle_z: 
     return output
 
 
-def render_shape(shape: list[Point], color: tuple[int, int, int], x_offset: int, y_offset: int) -> None:
+def render_shape(shape: list[Point], x_offset: int, y_offset: int) -> None:
     sorted_shape = sorted(shape, key=lambda x: x.z, reverse=True)
     for point in sorted_shape:
         x = point.x * K1 / K2 + point.z
         y = point.y * K1 / K2 + point.z
         luminance = point.luminance + 1.5
-        r = luminance * (color[0] / 3)
-        g = luminance * (color[1] / 3)
-        b = luminance * (color[2] / 3)
+        r = luminance * (point.color[0] / 3)
+        g = luminance * (point.color[1] / 3)
+        b = luminance * (point.color[2] / 3)
         pygame.draw.rect(win, (r, g, b), (x + x_offset, y + y_offset, PIXEL_SIZE, PIXEL_SIZE))
 
 
@@ -136,7 +137,7 @@ def main():
             angle_z = angle_z if angle_z <= 360 else 0
 
         rotated_torus = rotate_points(torus, angle_x, angle_y, angle_z)
-        render_shape(rotated_torus, COLOR, 400, 400)
+        render_shape(rotated_torus, 400, 400)
 
         # DISPLAY UPDATE
         pygame.display.update()
